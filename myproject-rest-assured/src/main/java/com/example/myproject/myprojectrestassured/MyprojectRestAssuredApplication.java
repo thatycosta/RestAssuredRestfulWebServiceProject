c1554c6808fb1cf9ee9815c804c41545bb9ee65f
Tauane Costa <tauanedasilvacosta@gmail.com>
2020-06-23 18:29:20 -0300

    First commit

---
+++ myproject-rest-assured/src/main/java/com/example/myproject/myprojectrestassured/MyprojectRestAssuredApplication.java
@@ -0,0 +1,13 @@
+package com.example.myproject.myprojectrestassured;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectRestAssuredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectRestAssuredApplication.class, args);
+	}
+
+}
---
+++ myproject-rest-assured/src/test/java/com/example/myproject/myprojectrestassured/MyprojectRestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myproject.myprojectrestassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyprojectRestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
