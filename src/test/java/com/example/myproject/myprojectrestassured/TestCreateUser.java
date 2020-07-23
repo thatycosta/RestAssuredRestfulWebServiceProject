a22f7d3bcccdaf3c8c872ef61c58a1691238bd56
Tauane Costa <tauanedasilvacosta@gmail.com>
2020-07-23 18:25:58 -0300

    inserting new asserts to teste create user

--- src/test/java/com/example/myproject/myprojectrestassured/TestCreateUser.java
+++ src/test/java/com/example/myproject/myprojectrestassured/TestCreateUser.java
@@ -3,6 +3,9 @@ package com.example.myproject.myprojectrestassured;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,8 +15,7 @@ import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCreateUser {
 
@@ -41,8 +43,8 @@ public class TestCreateUser {
 
         Map<String, Object> userDetails = new HashMap<>();
         userDetails.put("firstName", "Te");
-        userDetails.put("lastName", "ta");
-        userDetails.put("email", "ta@g.com");
+        userDetails.put("lastName", "tauane");
+        userDetails.put("email", "tauane@g.com");
         userDetails.put("password", "123");
         userDetails.put("addresses", userAddresses);
 
@@ -61,6 +63,24 @@ public class TestCreateUser {
 
         String userId = response.jsonPath().getString("userId");
         assertNotNull(userId);
+        assertTrue(userId.length() == 30);
+
+        String bodyString = response.getBody().asString();
+        try {
+            JSONObject responseBodyJson = new JSONObject(bodyString);
+            JSONArray addresses = responseBodyJson.getJSONArray("addresses");
+            assertNotNull(addresses);
+            assertTrue(addresses.length() == 1);
+
+            String addressId = addresses.getJSONObject(0).getString("addressId");
+            assertNotNull(addressId);
+            assertTrue(addressId.length() == 30);
+
+
+        } catch (JSONException e) {
+            fail(e.getMessage());
+        }
+
 
     }
 
